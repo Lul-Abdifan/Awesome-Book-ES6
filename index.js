239d57ffe261b6dfe44f1f7a777d254c118d9f61
A <abdibekele94@gmail.com>
2022-11-08 05:40:37 -0500

    Delete unnecessary code

--- index.js
+++ index.js
@@ -6,29 +6,6 @@ import Navigation from './modules/navigation.js'
 
 
 
-// display Books
-
-
-// Event:Add a Book
-document.querySelector('#add-new-book').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const title = document.querySelector('#title').value;
-  const author = document.querySelector('#author').value;
-  // instantiate
-
-  const book = new Collection(title, author);
-  // Add book to list
-  Page.addToPage(book);
-  Store.addBook(book);
-  document.querySelector('#title').value = '';
-  document.querySelector('#author').value = '';
-  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
-});
-
-// Event :Remove a Book
-
-
-//           //navigation for the all lists
 
 
 
